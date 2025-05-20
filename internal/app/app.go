42c15ce8a6bff49721b2d9f5bb90d10e824af355
Hubbal Kholiq Habbaza <habbazahubbal@gmail.com>
2025-05-20 15:18:47 +0700

    first commit

---
+++ internal/app/app.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Run() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	err := r.Run(":8080")
+	if err != nil {
+		log.Fatal("Failed to run server: ", err)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/habbazettt/book-review-go/internal/app"
+
+func main() {
+	app.Run()
+}
